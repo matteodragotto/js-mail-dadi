281631d9f321419e20adcaafb7da09e7422e380d
matteodragotto <matteodrag8@gmail.com>
2024-11-18 14:39:23 +0100

    [feat] dice game

--- js/script.js
+++ js/script.js
@@ -14,8 +14,23 @@ if (isEmailVerified === true) {
 
 console.log(messageVerified);
 
-const randomNumber = Math.random() * 100
-const playerNumber = prompt('Il tuo numero è ${randomNumber}')
-const cpuNumber = prompt('Il numero della CPU è ${randomNumber}')
+const randomNumber1 = Math.ceil(Math.random() * 100)
+const playerNumber = alert(`Il tuo numero è: ${randomNumber1}`)
 
+const randomNumber2 = Math.ceil(Math.random() * 100)
+const cpuNumber = alert(`Il numero della CPU è: ${randomNumber2}`)
+
+console.log(randomNumber1, randomNumber2)
+
+
+let winnerMessage;
+if (randomNumber1 > randomNumber2) {
+  winnerMessage = 'Il giocatore ha vinto!'
+} else if (randomNumber2 > randomNumber1) {
+  winnerMessage = 'La CPU ha vinto!'
+} else {
+  winnerMessage = 'Pareggio'
+}
+
+console.log(winnerMessage);
 
