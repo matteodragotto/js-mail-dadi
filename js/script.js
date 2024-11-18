dd6efbed2e1b8489f4a21e52f159155f3b2417de
matteodragotto <matteodrag8@gmail.com>
2024-11-18 14:58:24 +0100

    [feat] check for email prompt being an email

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const email = prompt('Inserisci la tua mail')
 const emailList = ['matteo@boolean.it', 'email2@boolean.it']
 const isEmailVerified = emailList.includes(email)
+const isEmailAnEmail = email.includes('@')
 console.log(emailList, isEmailVerified);
 
 
 let messageVerified;
-
-if (isEmailVerified === true) {
-  messageVerified = 'La mail è stata verificata con successo'
+if (isEmailVerified === true && isEmailAnEmail === true) {
+  messageVerified = alert('La mail è stata verificata con successo')  
 } else {
-  messageVerified = 'La mail non ha i permessi di accesso necessari'
+  messageVerified = alert('La mail inserita non è valida')
 }
 
 console.log(messageVerified);
